fb20d272d637bb36afa1fd9cdca33ac4af08c862
Joshua Pedraza <joshua.pedraza@educa.madrid.org>
2025-04-22 19:47:36 +0200

    Refactorizado el codigo

--- src/practicajoshuapedraza/CalculosMatematicos.java
+++ src/practicajoshuapedraza/CalculosMatematicos.java
@@ -7,44 +7,51 @@ public class CalculosMatematicos {
 	public CalculosMatematicos() {		
 	}
 
-	public void calculos(int numero) {
+	public void calculosRefactorizados(int numero) {
 		
 		
-		// Calculamos los factoriales primos
-		ArrayList<Integer> miArray = new ArrayList();
-		boolean noEsPrimo = false;
-		int j;
+		boolean noEsPrimo = factorialesprimosRefactorizado(numero);
 		
-		//Añadimos el 1
-		miArray.add(1);
-		
-		for (int  i = 2; i< numero ; i++) {
-			noEsPrimo = false;
-			j = 2;
-			while(j<i) {
-				if(i%j==0) {
-					noEsPrimo = true;
-					j=i;				
-				} else j++;
-			}
-			if(!noEsPrimo&&numero%i==0) {
-				miArray.add(i);								
-			}
-			
-		}			
-		
-		System.out.println("Los factoriales primos del número introducido son: "+miArray);
-		
-		for (int  i = 2; i< numero ; i++) {
-			noEsPrimo = false;			
-			if(numero%i==0) {
-				noEsPrimo = true;				
-				i=numero;
-			}
-		}
+        noEsPrimo = noEsPrimoRefactorizado(numero, noEsPrimo);
 		
 		if(noEsPrimo) {
 			System.out.println("El número "+numero+ " NO es primo");
 		} else System.out.println("El número "+numero+ " SI es primo");
 	}			
+
+    private boolean noEsPrimoRefactorizado(int numero, boolean noEsPrimo) {
+        for (int  i = 2; i< numero ; i++) {
+            noEsPrimo = false;
+            if(numero%i==0) {
+                noEsPrimo = true;
+                i=numero;
+            }
+        }
+        return noEsPrimo;
+    }
+
+    private boolean factorialesprimosRefactorizado(int numero) {
+        // Calculamos los factoriales primos
+        ArrayList<Integer> miArray = new ArrayList();
+        boolean noEsPrimo = false;
+        int j;
+        //Añadimos el 1
+        miArray.add(1);
+        for (int  i = 2; i< numero ; i++) {
+            noEsPrimo = false;
+            j = 2;
+            while(j<i) {
+                if(i%j==0) {
+                    noEsPrimo = true;
+                    j=i;
+                } else j++;
+            }
+            if(!noEsPrimo&&numero%i==0) {
+                miArray.add(i);
+            }
+            
+        }
+        System.out.println("Los factoriales primos del número introducido son: "+miArray);
+        return noEsPrimo;
+    }
 }
